2eb2d0b1d2b3c835d8ab3256da1cd8f097f9adff
Terren Culberson <culbersonterren@yahoo.com>
2020-01-24 04:28:44 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -6,7 +6,7 @@ class Waiter
   
    def initialize(name, yrs_experience)
      @name = name
-     @year_experience = yrs_experience
+     @yrs_experience = yrs_experience
      @@all << self 
    end
      
