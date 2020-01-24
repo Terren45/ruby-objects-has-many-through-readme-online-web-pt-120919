abc09beabb3cc63be9f9e3d6e441bddf0a0f232c
Terren Culberson <culbersonterren@yahoo.com>
2020-01-24 04:33:55 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -2,10 +2,16 @@ class Meal
   
   attr_accessor :waiter, :customer, :total, :tip 
   
+  @@all = []
+  
   def initialize(waiter, customer, total, tip=0)
     @waiter = waiter
     @customer = customer
     @total = total
     @tip = tip=0
+    @@all << 
   end
+  
+  def self.all
+    @@all
 end
