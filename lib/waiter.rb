13419590549bc5b74c5eb56c41fc21615a173c74
Terren Culberson <culbersonterren@yahoo.com>
2020-01-24 04:06:59 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,4 +1,15 @@
 class Waiter
   
-
+  attr_accessor :name, :years_experience 
+  
+  @@all = [] 
+  
+   def initialize(name, years_experience)
+     @name = name
+     @year_experience = years_experience
+     @all << self
+     
+     def self.all
+       @@all
+     end
 end
